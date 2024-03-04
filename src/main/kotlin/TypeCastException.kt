afee34915354c6d9c57be41d66146e0f9cea2fad
erkan011 <erkancaliskan011@gmail.com>
2024-03-04 16:02:09 +0300

    TypeCastException örnek olarak verilmiştir.

---
+++ src/main/kotlin/TypeCastException.kt
@@ -0,0 +1,22 @@
+package org.example
+
+//TypeCastException
+
+fun main() {
+
+    //hatasız kısım
+    val str = "123"
+    val num:Int=str.toInt()
+    println("str: $str")
+    println("num: $num")
+
+
+
+
+    //(TypeCastException.kt:9) Hatalı kısım
+    //val str2 = "abc"
+    //val num2:Int = str2.toInt()
+    //println("str2: $str2")
+    //println("num2: $num2")
+
+}
